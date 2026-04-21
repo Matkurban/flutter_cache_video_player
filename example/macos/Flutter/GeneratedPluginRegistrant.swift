6dfeea518ffb75466e0f3b3dee116bdd8f8b1a5f
kurban@windows <3496354336@qq.com>
2026-04-22 04:53:55 +0800

    fix:windows and linux bugs

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -2,12 +2,13 @@
 //  Generated file. Do not edit.
 //
 
-import connectivity_plus
-import flutter_cache_video_player
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import flutter_cache_video_player
+
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-    ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
-    FlutterCacheVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FlutterCacheVideoPlayerPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FlutterCacheVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FlutterCacheVideoPlayerPlugin"))
 }
